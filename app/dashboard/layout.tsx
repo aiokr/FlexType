df8fae411827af727a75612e1c2f75d6162846b4
aiokr <hiaiokr@gmail.com>
2024-04-02 09:47:30 +0800

    修改环境变量

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default async function DashboardLayout({ children, }: { children: React.R
   const loginUserEmail: string = session.user.email
   console.log(loginUserEmail)
   console.log(loginUserEmail)
-  const authedEmails = process.env.NEXT_PUBLIC_AUTHORIZED_EMAILS || ''; // 获取环境变量的值
+  const authedEmails = process.env.AUTHORIZED_EMAILS || ''; // 获取环境变量的值
   const authorizedEmailsArray = authedEmails.split(',').map(email => email.trim()); // 将以逗号分隔的字符串转换为字符串数组
   const isAuthorized = authorizedEmailsArray.includes(loginUserEmail); // 检查登录用户的邮箱是否在授权邮箱列表中
   if (isAuthorized) {
