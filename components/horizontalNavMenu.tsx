3f5d2878603ca78230712090e0a965b563160b14
aiokr <hiaiokr@gmail.com>
2024-04-09 22:57:24 +0800

    perf: 优化移动端导航栏显示

--- components/horizontalNavMenu.tsx
+++ components/horizontalNavMenu.tsx
@@ -4,11 +4,10 @@ import Link from 'next/link'
 
 export default function HorizontalNavMenu(data: any) {
   return (
-    <div className="flex flex-col gap-4 items-start px-12">
+    <div className="grid grid-cols-2 gap-4 items-start px-12">
       {data.data.map((item: any) => (
         <div key={item.itemKey} className="flex flex-col items-center">
           <Link href={item.itemKey} className='flex flex-row gap-6 text-sm font-bold'>
-            <i className='inline-flex w-6 h-6'>{item.icon}</i>
             {item.text}
           </Link>
         </div>
