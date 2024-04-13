4c0c53fc371ac653b0df9b70087e70cb2dab1116
aiokr <hiaiokr@gmail.com>
2024-04-13 23:52:33 +0800

    feat: 「照片流」功能基本完成

--- app/api/newPhoto/route.tsx
+++ app/api/newPhoto/route.tsx
@@ -1,5 +1,6 @@
 import AuthSession from '@/components/getAuthSession'
 import prisma from '@/libs/prisma'
+import { errorToJSON } from 'next/dist/server/render';
 
 export async function PUT(req: any, res: any) {
   const session = await AuthSession()
@@ -13,7 +14,52 @@ export async function PUT(req: any, res: any) {
   const userID = existingUser[0].id
 
   if (session && existingUser.length !== 0) {
-  
+    const flowItemData: any = await req.json()
+    console.log(flowItemData)
+    const info = Object.entries(flowItemData).reduce((accumulator: any, [key, value]) => {
+      if (value !== null) { // 只有当值不为null时，才将键值对添加到accumulator对象中
+        accumulator[key] = value;
+      }
+      return accumulator;
+    }, {});
+
+    let assertAlreadyExisting: boolean = null
+
+    // check if assetId already exists
+    const findAlreadyExistingAssert = await prisma.photo.findMany({
+      where: {
+        assetId: flowItemData.assetId
+      }
+    })
+    if (findAlreadyExistingAssert.length !== 0) {
+      assertAlreadyExisting = true
+    }
+
+    console.log(flowItemData.id !== null, assertAlreadyExisting)
+    if (flowItemData.id !== null) { // 项目已存在，修改项目
+      const updateFlowItem = await prisma.photo.update({
+        where: {
+          id: flowItemData.id
+        },
+        data: {
+          assetId: parseInt(flowItemData.assetId),
+          title: flowItemData.title,
+          info: info
+        }
+      })
+      return Response.json({ message: "Success Update Item" }, { status: 200 })
+    } else if (flowItemData.id == null && !assertAlreadyExisting) { // 项目不存在，创建项目
+      const writeFlowItem = await prisma.photo.create({
+        data: {
+          assetId: parseInt(flowItemData.assetId),
+          title: flowItemData.title,
+          info: info
+        }
+      })
+      return Response.json({ message: "Success Create New Item" }, { status: 200 })
+    } else {
+      return Response.json({ message: "Other error" }, { status: 400 })
+    }
   } else {
     return Response.json({ message: "Not authenticated" }, { status: 403 })
   }
--- components/photoList.tsx
+++ components/photoList.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import React, { useState, useEffect } from 'react';
 import { Table, Input, Toast, Select, Modal, Button, Card } from '@douyinfe/semi-ui';
 import Image from 'next/image'
@@ -12,6 +11,7 @@ interface PhotoListProps {
 }
 
 interface SelectedPhoto {
+  assetId: string | null;
   id: string | null;
   title: string | null;
   url: string | null;
@@ -31,7 +31,8 @@ const PhotoListComponent: React.FC<PhotoListProps> = ({ photosData, assertsData 
   useEffect(() => { setData(photosData); }, [photosData]);
   const [assertData, setAssertData] = useState(assertsData);  // Assets 列表的数据
   useEffect(() => { setAssertData(assertsData); }, [assertsData]);
-  const [selected, setSelected] = useState<SelectedPhoto>({
+  const [selected, setSelected] = useState<SelectedPhoto>({ // 选中照片的数据
+    assetId: null,
     id: null,
     title: null,
     url: null,
@@ -49,29 +50,30 @@ const PhotoListComponent: React.FC<PhotoListProps> = ({ photosData, assertsData 
   const showDialog = () => {
     setVisible(true);
   };
+
+  // 点击弹窗的确定，清理数据，然后进行上传操作
   const handleOk = () => {
     // 根据选中的 id 查找原始资产
-    const originalAsset = assertsData.find((item: any) => item.assetId === selected.id);
-    console.log(originalAsset)
+    const originalAsset = assertsData.find((item: any) => item.assetId === selected.assetId);
 
     // 创建一个新对象，如果值未更改，则设置为 null
     const cleanedSelected = Object.keys(selected).reduce((acc: any, key) => {
+      console.log(selected.assetId)
       // 断言 key 为 SelectedPhoto 的键
       const selectedKey = key as keyof SelectedPhoto;
       // 检查原始资产是否存在，以及 selected[key] 是否与之不同
       const hasChanged = originalAsset && (selected[selectedKey] !== originalAsset[selectedKey]);
       // 如果值未更改，则设置为 null，否则保留当前值
       acc[selectedKey] = hasChanged ? selected[selectedKey] : null;
+      acc.assetId = selected.assetId
+      acc.id = selected.id
+      console.log(acc);
       return acc;
     }, {} as SelectedPhoto);
 
-    // 现在您有了一个清理过的对象，未更改的值被设置为 null
-    const json = JSON.stringify(cleanedSelected);
-    // 对 json 做一些操作...
-    console.log(json);
-
+    const json = JSON.stringify(cleanedSelected); // 清理后的 SelectedPhoto 对象
+    newFlowItem(json);  // 调用新建操作
     setVisible(false);
-    console.log('Ok 按钮被点击');
   };
   const handleCancel = () => {
     setVisible(false);
@@ -89,12 +91,13 @@ const PhotoListComponent: React.FC<PhotoListProps> = ({ photosData, assertsData 
     <>{optionNode.label}</>
   )
 
+  // 选中项改变
   const selectOnChange = (value: any) => {
     const selectedAsset = assertData.find((item: any) => item.assetId === value);
     if (selectedAsset) {
-      setSelected({
-        id: selectedAsset.assetId,
-        title: selectedAsset.title,
+      setSelected(prevSelected => ({
+        ...prevSelected,
+        assetId: selectedAsset.assetId,
         url: selectedAsset.url,
         width: selectedAsset.width,
         height: selectedAsset.height,
@@ -103,44 +106,90 @@ const PhotoListComponent: React.FC<PhotoListProps> = ({ photosData, assertsData 
         Model: selectedAsset.Model,
         LensMake: selectedAsset.LensMake,
         LensModel: selectedAsset.LensModel
-      });
+        // 注意：这里没有更新 title 字段
+      }));
     }
   };
 
+  // 写入选中照片
   const setPhoto = (event: React.ChangeEvent<HTMLInputElement>, field: keyof SelectedPhoto) => {
-    const newValue = event
     setSelected(prevSelected => ({
       ...prevSelected,
-      [field]: newValue
+      [field]: event
     }));
   };
 
-  const newPhoto = () => {
+  // 处理新建照片流项目的逻辑
+  const newFlowItem = async (json: string) => {
+    try {
+      const response = await fetch('/api/newphoto', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: json
+      })
+      if (response.ok) {
+        Toast.success('New flow item created successfully.' + json)
+      } else {
+        Toast.error('Error creating new flow item.')
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  // 处理删除照片的逻辑
+  const deleteItem = async (id: string) => {
+    try {
+      const response = await fetch(`/api/delete/photo/${id}`, {
+        method: 'DELETE'
+      })
+      if (response.ok) {
+        Toast.success(`File ID ${id} deleted successfully.`)
+        setVisible(false);
+      } else {
+        Toast.error('Error deleting file.')
+        setVisible(false);
+      }
+    } catch (error) {
+      console.error(error)
+      setVisible(false);
+    }
+  }
+
+  // Open PhotoFlow Item Editor
+  const openPhotoEditor = (data: any) => {
+    // 如果 data 为 null，说明是创建新照片，否则是编辑现有照片
+    const newData = data || {
+      id: null,
+      assetId: null,
+      title: null,
+      url: null,
+      width: null,
+      height: null,
+      DateTimeOriginal: null,
+      Make: null,
+      Model: null,
+      LensMake: null,
+      LensModel: null
+    };
+    setSelected(newData);
     setVisible(true);
   }
 
   return (
     <>
       <div className='grid grid-cols-2 lg:grid-cols-3 gap-2'>
-        <button className='border rounded-[6px] w-full h-full' onClick={newPhoto}>
+        <button className='border rounded-[6px] w-full h-full' onClick={() => openPhotoEditor(null)}>新建项目
         </button>
         {data.map((item: any) => (
-          <Card key={item.id}
-            cover={<Image src={item.url} alt={item.title} height={item.height} width={item.width} unoptimized />}
-          >
-            <Meta title={item.title || 'No Title'} />
-          </Card>
+          <button key={item.id} onClick={() => openPhotoEditor(item)}>
+            <Card cover={<Image src={item.url} alt={item.title} height={item.height} width={item.width} className='aspect-[4/3] object-cover' unoptimized />}>
+              <Meta title={item.title || 'No Title'} />
+            </Card>
+          </button>
         ))}
-        <Modal
-          title="新建图片"
-          visible={visible}
-          onOk={handleOk}
-          afterClose={handleAfterClose} //>=1.16.0
-          onCancel={handleCancel}
-          closeOnEsc={true}
-          closable={false}
-          fullScreen
-        >
+        <Modal title="新建图片" visible={visible} fullScreen
+          onOk={handleOk} onCancel={handleCancel} closeOnEsc={true} closable={false} afterClose={handleAfterClose}>
           <div className='container mx-auto max-w-[800px]'>
             <div>
               <Select
@@ -166,13 +215,15 @@ const PhotoListComponent: React.FC<PhotoListProps> = ({ photosData, assertsData 
               </Select>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-4 gap-4 mt-8'>
-              <Input size='large' prefix="ID*" className='col-span-1' value={selected.id || ''} onChange={(event: any) => setPhoto(event, 'id')} ></Input>
-              <Input size='large' prefix="Title*" className='col-span-4' onChange={(event: any) => setPhoto(event, 'title')} ></Input>
+              <Input size='large' prefix="PhotoFlowID*" className='col-span-2' value={selected.id || ''} disabled></Input>
+              <Input size='large' prefix="AssertsID*" className='col-span-2' value={selected.assetId || ''} onChange={(event: any) => setPhoto(event, 'assetId')} ></Input>
+              <Input size='large' prefix="Title*" className='col-span-4' value={selected.title || ''} onChange={(event: any) => setPhoto(event, 'title')} ></Input>
               <Input size='large' placeholder={selected.Make} className='col-span-1' prefix="Make*" onChange={(event: any) => setPhoto(event, 'Make')}  ></Input>
               <Input size='large' placeholder={selected.Model} className='col-span-3' prefix="Model*" onChange={(event: any) => setPhoto(event, 'Model')} ></Input>
               <Input size='large' placeholder={selected.LensMake || ''} className='col-span-3' prefix="LensMake*" onChange={(event: any) => setPhoto(event, 'LensMake')}></Input>
               <Input size='large' placeholder={selected.LensModel} className='col-span-3' prefix="LensModel*" onChange={(event: any) => setPhoto(event, 'LensModel')}></Input>
             </div>
+            <button className='bg-red-500 text-white py-2 px-4 my-6 rounded' onClick={() => deleteItem(selected.id)}>删除</button>
           </div>
         </Modal>
       </div>
