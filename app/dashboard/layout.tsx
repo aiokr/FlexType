2ad47eb344a0b7104f235b754c0d933c85109427
aiokr <hiaiokr@gmail.com>
2024-05-22 11:26:24 +0800

    fix: 修复鉴权问题

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,19 +9,20 @@ import {Header, Content} from 'antd/es/layout/layout'
 export default async function DashboardLayout({children}: {children: React.ReactNode}) {
   const supabase = createClient()
   const {data, error} = await supabase.auth.getUser()
-  const loginUserData = (await createClient().auth.getUser()).data
-  const userData = await prisma.user.findUnique({where: {uid: data.user?.id}})
-  let loginUserAvatar: string = '/icon.png'
-  if (loginUserData.user !== null) {
-    const userId = loginUserData.user?.id
-    const userData = await prisma.user.findUnique({where: {uid: userId}})
-    loginUserAvatar = userData.image
-  } else {
-    loginUserAvatar = '/icon.png'
-  }
+  console.log(data)
 
   if (error || !data?.user) {
     redirect('/login')
+  }
+
+  const loginUserData = (await createClient().auth.getUser()).data
+  const userData = await prisma.user.findUnique({where: {uid: data.user?.id}})
+  let loginUserAvatar: string = '/icon.png'
+
+  if (loginUserData.user !== null) {
+    loginUserAvatar = userData.image
+  } else {
+    loginUserAvatar = '/icon.png'
   }
 
   return (
