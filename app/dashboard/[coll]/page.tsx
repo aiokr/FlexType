4407f1705ac80aa8a4b49bb4adb2a856859c1e2f
aiokr <hiaiokr@gmail.com>
2024-05-26 23:00:21 +0800

    feat: 内容集首页支持所有内容集合并显示

--- app/dashboard/[coll]/page.tsx
+++ app/dashboard/[coll]/page.tsx
@@ -29,32 +29,52 @@ export default async function CollecntionHomePage({params}: {params: {coll: stri
     }
   ]
 
-  const collectionData = await prisma.collection.findUnique({
-    where: {
-      slug: params.coll
-    },
-    include: {
-      authorizedUser: {
-        include: {
-          authorizedUser: true
+  let collectionData = null
+
+  if (params.coll === 'all') {
+    // 获取所有内容集
+    let preCollection = await prisma.collection.findMany({
+      include: {
+        post: true,
+        photo: true,
+        authorizedUser: {
+          include: {
+            authorizedUser: true
+          }
         }
-      },
-      post: true,
-      photo: true
+      }
+    })
+
+    // 合并每个内容集的数据
+    collectionData = {
+      post: preCollection.map((item) => item.post).flat(),
+      photo: preCollection.map((item) => item.photo).flat(),
+      authorizedUser: preCollection.map((item) => item.authorizedUser).flat()
     }
-  })
-
-  // console.log(collectionData)
-
-  if (!collectionData) {
-    redirect('/dashboard')
+  } else {
+    collectionData = await prisma.collection.findUnique({
+      where: {
+        slug: params.coll
+      },
+      include: {
+        authorizedUser: {
+          include: {
+            authorizedUser: true
+          }
+        },
+        post: true,
+        photo: true
+      }
+    })
   }
+
+  console.log(collectionData)
 
   return (
     <div className="pt-14 md:pt-0 mt-4 w-full">
       <Breadcrumb items={breadcrumbItem} className="inline-block text-xs" />
       <div className="container max-w-[1200px] mx-auto">
-        <div className="pt-12 text-3xl font-bold">{collectionData.name}</div>
+        <div className="pt-12 text-3xl font-bold">{collName}</div>
         <section className="w-full grid grid-cols-2 md:grid-cols-4 gap-4 pt-6">
           <Card bordered={false}>
             <Statistic title="Post" value={collectionData.post.length} valueStyle={{color: '#222831'}} />
